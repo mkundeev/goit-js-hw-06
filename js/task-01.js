8f5f5a927e5e6291d5ff2eafe3f5ab55ed57162f
MaksymKundeev <mkundeev@gmail.com>
2022-05-22 22:27:18 +0300

    1

--- js/task-01.js
+++ js/task-01.js
@@ -1,16 +1,16 @@
 
-const listEl = document.querySelector('#categories');
+
 const listItemEl = document.querySelectorAll('.item');
 
-function getNumberOfCategories(list) {
-    console.log(`Number of categories: ${list.children.length}`);
+function getNumberOfCategories(listOfItems) {
+    console.log(`Number of categories: ${listOfItems.length}`);
 };
 
 function getListOfCategories(listOfItems) {
-    return [...listOfItems].forEach((element) => {
+    return listOfItems.forEach((element) => {
         console.log(`Category: ${element.firstElementChild.textContent}`);
         console.log(`Elements: ${element.lastElementChild.children.length}`);
     });
 }
-getNumberOfCategories(listEl);
+getNumberOfCategories(listItemEl);
 getListOfCategories(listItemEl);
